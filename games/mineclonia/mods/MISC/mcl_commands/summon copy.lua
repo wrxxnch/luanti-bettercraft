6a46b5383afcf9970a7c75028f894bbf992582b1
root <root@Sheila.localdomain>
2026-01-18 23:30:00 -0300

    args de comando summon adicionados

--- games/mineclonia/mods/MISC/mcl_commands/summon copy.lua
+++ games/mineclonia/mods/MISC/mcl_commands/summon copy.lua
@@ -1,15 +1,196 @@
 local S = core.get_translator(core.get_current_modname())
 
-local orig_func = core.registered_chatcommands["spawnentity"].func
-local cmd = table.copy(core.registered_chatcommands["spawnentity"])
+-- =====================================================
+-- BASE
+-- =====================================================
+local base_cmd = core.registered_chatcommands["spawnentity"]
+assert(base_cmd, "[summon] spawnentity não encontrado")
+
+local cmd = table.copy(base_cmd)
+cmd.params = S("<entidade> [param=valor] ...")
+cmd.description = S("Invoca entidades com parâmetros avançados")
+
+-- =====================================================
+-- FUNÇÃO PRINCIPAL
+-- =====================================================
 cmd.func = function(name, param)
-	local ent = core.registered_entities[param]
-	if core.settings:get_bool("only_peaceful_mobs", false) and ent and ent.is_mob and ent.type == "monster" then
-		return false, S("Only peaceful mobs allowed!")
-	else
-		local bool, msg = orig_func(name, param)
-		return bool, msg
+	if param == "" or not param then
+		return false, S("Uso: /summon <entidade> [param=valor]...")
 	end
+
+	local args = param:split(" ")
+	local entname = table.remove(args, 1)
+
+	-- Resolve nome curto
+	if not entname:find(":") then
+		if core.registered_entities["mobs_mc:" .. entname] then
+			entname = "mobs_mc:" .. entname
+		end
+	end
+
+	local ent_def = core.registered_entities[entname]
+	if not ent_def then
+		return false, S("Entidade desconhecida: @1", entname)
+	end
+
+	if core.settings:get_bool("only_peaceful_mobs", false)
+	and ent_def.is_mob
+	and ent_def.type == "monster" then
+		return false, S("Apenas mobs pacíficos são permitidos!")
+	end
+
+	local player = core.get_player_by_name(name)
+	if not player then
+		return false, S("Jogador não encontrado.")
+	end
+
+	local pos = vector.add(player:get_pos(), {x=0, y=1.5, z=0})
+
+	local obj = core.add_entity(pos, entname)
+	if not obj then
+		return false, S("Falha ao criar a entidade: @1", entname)
+	end
+
+	local luaent = obj:get_luaentity()
+
+	-- =====================================================
+	-- PARÂMETROS
+	-- =====================================================
+	for _, arg in ipairs(args) do
+		local key, value = arg:match("([^=]+)=(.+)")
+		if not key then
+			goto continue
+		end
+
+		local final = value
+		if value == "true" then final = true end
+		if value == "false" then final = false end
+		if tonumber(value) then final = tonumber(value) end
+
+		--------------------------------------------------
+		-- CHILD
+		--------------------------------------------------
+		if key == "child" and luaent then
+			luaent.child = final
+			if luaent.set_child then
+				luaent:set_child(final)
+			end
+
+		--------------------------------------------------
+		-- PASSIVE
+		--------------------------------------------------
+		elseif key == "passive" and luaent then
+			luaent.passive = final
+
+		--------------------------------------------------
+		-- TAMED
+		--------------------------------------------------
+		elseif key == "tamed" and luaent then
+			luaent.tamed = final
+			luaent.owner = final and name or nil
+
+		--------------------------------------------------
+		-- NAMETAG
+		--------------------------------------------------
+		elseif key == "nametag" then
+			obj:set_nametag_attributes({ text = tostring(final) })
+
+		--------------------------------------------------
+		-- HP
+		--------------------------------------------------
+		elseif key == "hp" then
+			obj:set_hp(tonumber(final) or obj:get_hp())
+
+		--------------------------------------------------
+		-- YAW
+		--------------------------------------------------
+		elseif key == "yaw" then
+			obj:set_yaw(math.rad(tonumber(final) or 0))
+
+		--------------------------------------------------
+		-- HAND (FORÇADO – SEM EXCEÇÃO)
+		--------------------------------------------------
+		elseif key == "hand" and luaent then
+			-- Mineclonia usa APENAS este método
+			if mcl_mobs and mcl_mobs.set_wielded_item then
+				mcl_mobs.set_wielded_item(luaent, ItemStack(final))
+				luaent._summon_forced_item = final -- trava visual
+			end
+
+		--------------------------------------------------
+		-- ARMOR
+		--------------------------------------------------
+		elseif key == "armor" and luaent and luaent.set_armor_groups then
+			luaent:set_armor_groups({ fleshy = tonumber(final) or 100 })
+
+		--------------------------------------------------
+		-- RIDE
+		--------------------------------------------------
+		elseif key == "ride" then
+			local ride_name = value
+
+			if not ride_name:find(":") then
+				if core.registered_entities["mobs_mc:" .. ride_name] then
+					ride_name = "mobs_mc:" .. ride_name
+				end
+			end
+
+			if core.registered_entities[ride_name] then
+				local mount = core.add_entity(pos, ride_name)
+				if mount then
+					obj:set_attach(mount, "", vector.zero(), vector.zero())
+				end
+			end
+
+		--------------------------------------------------
+		-- FALLBACK
+		--------------------------------------------------
+		else
+			pcall(function()
+				obj:set_properties({ [key] = final })
+			end)
+		end
+
+		::continue::
+	end
+
+	return true, S("Entidade invocada: @1", entname)
 end
-core.unregister_chatcommand("spawnentity")
+
+-- =====================================================
+-- REGISTRO
+-- =====================================================
+if core.registered_chatcommands["spawnentity"] then
+	core.unregister_chatcommand("spawnentity")
+end
 core.register_chatcommand("summon", cmd)
+
+-- =====================================================
+-- HELP
+-- =====================================================
+core.register_chatcommand("summon_help", {
+	description = S("Ajuda completa do /summon"),
+	func = function(name)
+		core.chat_send_player(name, [[
+/summon <entidade> [param=valor]
+
+PARÂMETROS:
+child=true|false
+passive=true|false
+tamed=true|false
+nametag=Texto
+hp=NUM
+yaw=GRAUS
+hand=itemstring   (FORÇADO)
+armor=NUM
+ride=entidade
+
+EXEMPLOS:
+/summon zombie hand=mcl_core:dirt
+/summon skeleton hand=mcl_core:bow
+/summon zombie ride=spider
+/summon piglin hand=mcl_core:sword_gold
+]])
+		return true
+	end
+})
--- games/mineclonia/mods/MISC/mcl_commands/summon.lua
+++ games/mineclonia/mods/MISC/mcl_commands/summon.lua
@@ -1,196 +1,152 @@
-local S = core.get_translator(core.get_current_modname())
+minetest.register_chatcommand("summon", {
+	params = "<mob> [args]",
+	description = "Invoca um mob com parâmetros (ex: /summon creeper hp=100,name=Boss,glow=10)",
+	privs = { server = true },
 
--- =====================================================
--- BASE
--- =====================================================
-local base_cmd = core.registered_chatcommands["spawnentity"]
-assert(base_cmd, "[summon] spawnentity não encontrado")
-
-local cmd = table.copy(base_cmd)
-cmd.params = S("<entidade> [param=valor] ...")
-cmd.description = S("Invoca entidades com parâmetros avançados")
-
--- =====================================================
--- FUNÇÃO PRINCIPAL
--- =====================================================
-cmd.func = function(name, param)
-	if param == "" or not param then
-		return false, S("Uso: /summon <entidade> [param=valor]...")
-	end
-
-	local args = param:split(" ")
-	local entname = table.remove(args, 1)
-
-	-- Resolve nome curto
-	if not entname:find(":") then
-		if core.registered_entities["mobs_mc:" .. entname] then
-			entname = "mobs_mc:" .. entname
-		end
-	end
-
-	local ent_def = core.registered_entities[entname]
-	if not ent_def then
-		return false, S("Entidade desconhecida: @1", entname)
-	end
-
-	if core.settings:get_bool("only_peaceful_mobs", false)
-	and ent_def.is_mob
-	and ent_def.type == "monster" then
-		return false, S("Apenas mobs pacíficos são permitidos!")
-	end
-
-	local player = core.get_player_by_name(name)
-	if not player then
-		return false, S("Jogador não encontrado.")
-	end
-
-	local pos = vector.add(player:get_pos(), {x=0, y=1.5, z=0})
-
-	local obj = core.add_entity(pos, entname)
-	if not obj then
-		return false, S("Falha ao criar a entidade: @1", entname)
-	end
-
-	local luaent = obj:get_luaentity()
-
-	-- =====================================================
-	-- PARÂMETROS
-	-- =====================================================
-	for _, arg in ipairs(args) do
-		local key, value = arg:match("([^=]+)=(.+)")
-		if not key then
-			goto continue
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		if not player then
+			return false, "Jogador não encontrado"
 		end
 
-		local final = value
-		if value == "true" then final = true end
-		if value == "false" then final = false end
-		if tonumber(value) then final = tonumber(value) end
+		if param == "" then
+			return false, "Uso: /summon <mob> [args]"
+		end
 
-		--------------------------------------------------
-		-- CHILD
-		--------------------------------------------------
-		if key == "child" and luaent then
-			luaent.child = final
-			if luaent.set_child then
-				luaent:set_child(final)
-			end
+		-- separa nome do mob e argumentos
+		local mobname, argstr = param:match("^(%S+)%s*(.*)$")
 
-		--------------------------------------------------
-		-- PASSIVE
-		--------------------------------------------------
-		elseif key == "passive" and luaent then
-			luaent.passive = final
+		-- normaliza nome (igual minecraft)
+		if not mobname:find(":") then
+			mobname = "mobs_mc:" .. mobname
+		end
 
-		--------------------------------------------------
-		-- TAMED
-		--------------------------------------------------
-		elseif key == "tamed" and luaent then
-			luaent.tamed = final
-			luaent.owner = final and name or nil
+		local pos = vector.round(player:get_pos())
+		pos.y = pos.y + 1
 
-		--------------------------------------------------
-		-- NAMETAG
-		--------------------------------------------------
-		elseif key == "nametag" then
-			obj:set_nametag_attributes({ text = tostring(final) })
+		local obj = minetest.add_entity(pos, mobname)
+		if not obj then
+			return false, "Falha ao spawnar mob: " .. mobname
+		end
 
-		--------------------------------------------------
-		-- HP
-		--------------------------------------------------
-		elseif key == "hp" then
-			obj:set_hp(tonumber(final) or obj:get_hp())
+		local mob = obj:get_luaentity()
+		if not mob then
+			obj:remove()
+			return false, "Entidade não é um mob válido"
+		end
 
-		--------------------------------------------------
-		-- YAW
-		--------------------------------------------------
-		elseif key == "yaw" then
-			obj:set_yaw(math.rad(tonumber(final) or 0))
-
-		--------------------------------------------------
-		-- HAND (FORÇADO – SEM EXCEÇÃO)
-		--------------------------------------------------
-		elseif key == "hand" and luaent then
-			-- Mineclonia usa APENAS este método
-			if mcl_mobs and mcl_mobs.set_wielded_item then
-				mcl_mobs.set_wielded_item(luaent, ItemStack(final))
-				luaent._summon_forced_item = final -- trava visual
-			end
-
-		--------------------------------------------------
-		-- ARMOR
-		--------------------------------------------------
-		elseif key == "armor" and luaent and luaent.set_armor_groups then
-			luaent:set_armor_groups({ fleshy = tonumber(final) or 100 })
-
-		--------------------------------------------------
-		-- RIDE
-		--------------------------------------------------
-		elseif key == "ride" then
-			local ride_name = value
-
-			if not ride_name:find(":") then
-				if core.registered_entities["mobs_mc:" .. ride_name] then
-					ride_name = "mobs_mc:" .. ride_name
+		-- =========================
+		-- Parse dos argumentos
+		-- =========================
+		local args = {}
+		if argstr and argstr ~= "" then
+			for token in argstr:gmatch("[^,]+") do
+				local k, v = token:match("^([^=]+)=?(.*)$")
+				if k then
+					k = k:trim()
+					v = v:trim()
+					if v == "" or v == "true" then v = true
+					elseif v == "false" then v = false
+					elseif tonumber(v) then v = tonumber(v)
+					end
+					args[k] = v
 				end
 			end
-
-			if core.registered_entities[ride_name] then
-				local mount = core.add_entity(pos, ride_name)
-				if mount then
-					obj:set_attach(mount, "", vector.zero(), vector.zero())
-				end
-			end
-
-		--------------------------------------------------
-		-- FALLBACK
-		--------------------------------------------------
-		else
-			pcall(function()
-				obj:set_properties({ [key] = final })
-			end)
 		end
 
-		::continue::
-	end
+		-- =========================
+		-- APLICAR FLAGS E ATRIBUTOS
+		-- =========================
 
-	return true, S("Entidade invocada: @1", entname)
-end
+		-- Vida e Respiração
+		if args.hp then
+			mob.health = math.min(args.hp, mob.hp_max or args.hp)
+			obj:set_hp(mob.health)
+		end
+		if args.hp_max then mob.hp_max = args.hp_max end
+		if args.breath then mob.breath = args.breath end
+		if args.breath_max then mob.breath_max = args.breath_max end
 
--- =====================================================
--- REGISTRO
--- =====================================================
-if core.registered_chatcommands["spawnentity"] then
-	core.unregister_chatcommand("spawnentity")
-end
-core.register_chatcommand("summon", cmd)
+		-- Nome e Visual
+		if args.name then
+			mob.nametag = args.name
+			obj:set_properties({nametag = args.name})
+		end
+		if args.glow then
+			obj:set_properties({ glow = args.glow })
+		end
+		
+		-- Tamanho e Child
+		if args.child ~= nil then
+			mob.child = (args.child == true)
+			if mob.child and mob.base_visual_size then
+				obj:set_properties({
+					visual_size = {
+						x = mob.base_visual_size.x * 0.5,
+						y = mob.base_visual_size.y * 0.5
+					}
+				})
+			end
+		end
+		if args.scale then
+			local s = args.scale
+			obj:set_properties({ visual_size = {x=s, y=s} })
+		end
 
--- =====================================================
--- HELP
--- =====================================================
-core.register_chatcommand("summon_help", {
-	description = S("Ajuda completa do /summon"),
-	func = function(name)
-		core.chat_send_player(name, [[
-/summon <entidade> [param=valor]
+		-- Comportamento Base
+		if args.passive ~= nil then mob.passive = args.passive end
+		if args.retaliates ~= nil then mob.retaliates = args.retaliates end
+		if args.docile_by_day ~= nil then mob.docile_by_day = args.docile_by_day end
+		if args.day_docile ~= nil then mob.docile_by_day = args.day_docile end -- Alias do usuário
+		if args.persistent ~= nil then mob.persistent = args.persistent end
+		if args.persist_in_peaceful ~= nil then mob.persist_in_peaceful = args.persist_in_peaceful end
 
-PARÂMETROS:
-child=true|false
-passive=true|false
-tamed=true|false
-nametag=Texto
-hp=NUM
-yaw=GRAUS
-hand=itemstring   (FORÇADO)
-armor=NUM
-ride=entidade
+		-- Combate
+		if args.damage then mob.damage = args.damage end
+		if args.reach then mob.reach = args.reach end
+		if args.knock_back ~= nil then mob.knock_back = args.knock_back end
+		if args.armor then mob.armor = args.armor end
 
-EXEMPLOS:
-/summon zombie hand=mcl_core:dirt
-/summon skeleton hand=mcl_core:bow
-/summon zombie ride=spider
-/summon piglin hand=mcl_core:sword_gold
-]])
-		return true
+		-- Movimentação
+		if args.walk_velocity then mob.walk_velocity = args.walk_velocity end
+		if args.run_velocity then mob.run_velocity = args.run_velocity end
+		if args.jump ~= nil then mob.jump = args.jump end
+		if args.jump_height then mob.jump_height = args.jump_height end
+		if args.stepheight then mob.stepheight = args.stepheight end
+		if args.fly ~= nil then mob.fly = args.fly end
+		if args.swims ~= nil then mob.swims = args.swims end
+		if args.floats ~= nil then mob.floats = args.floats end
+		if args.view_range then mob.view_range = args.view_range end
+
+		-- Danos Ambientais
+		if args.water_damage then mob.water_damage = args.water_damage end
+		if args.lava_damage then mob.lava_damage = args.lava_damage end
+		if args.fire_damage then mob.fire_damage = args.fire_damage end
+		if args.light_damage then mob.light_damage = args.light_damage end
+		if args.suffocation ~= nil then mob.suffocation = args.suffocation end
+		if args.fall_damage ~= nil then mob.fall_damage = args.fall_damage end
+		if args.fear_height then mob.fear_height = args.fear_height end
+
+		-- Luz Solar (Específico para mortos-vivos)
+		if args.ignited_by_sunlight == false then
+			mob.ignited_by_sunlight = false
+			mob.sunlight_damage = 0
+			if mob.extinguish then mob:extinguish() end
+		end
+
+		-- Posse e Domesticação
+		if args.owner then
+			mob.owner = args.owner
+			mob.tamed = true
+		end
+		if args.tamed ~= nil then mob.tamed = args.tamed end
+		if args.order then mob.order = args.order end -- "follow" ou "stand"
+
+		-- Força aplicar propriedades e inicialização
+		if mob.on_spawn then
+			mob:on_spawn()
+		end
+
+		return true, "Mob spawnado: " .. mobname .. " com argumentos aplicados."
 	end
 })
