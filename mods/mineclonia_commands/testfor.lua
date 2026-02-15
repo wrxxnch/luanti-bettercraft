f2c9fbceb6a218423c312184549ad1bfaa0d2751
root <root@Sheila.localdomain>
2026-02-14 22:30:34 -0300

    added help to testforblocks command

--- mods/mineclonia_commands/testfor.lua
+++ mods/mineclonia_commands/testfor.lua
@@ -6,7 +6,6 @@
 -- /testforblocks
 -- compatível com Mineclonia
 --------------------------------------------------
-
 mineclonia_commands = mineclonia_commands or {}
 
 --------------------------------------------------
@@ -15,11 +14,11 @@ mineclonia_commands = mineclonia_commands or {}
 
 local function set_commandblock_result(success)
 
-    local pos =
-        core.commandblock_pos
-        or (minetest.get_commandblock_pos and minetest.get_commandblock_pos())
+    local pos = core.commandblock_pos or (minetest.get_commandblock_pos and minetest.get_commandblock_pos())
 
-    if not pos then return end
+    if not pos then
+        return
+    end
 
     local meta = minetest.get_meta(pos)
 
@@ -43,20 +42,20 @@ local function extract_conditional_commands(param)
 
     local function extract(key)
 
-        local s,e = param:find(key.."=")
+        local s, e = param:find(key .. "=")
 
-        if not s then return nil end
+        if not s then
+            return nil
+        end
 
-        local start = e+1
+        local start = e + 1
         local next = param:find(" execute", start)
 
-        local stop = next and next-1 or #param
+        local stop = next and next - 1 or #param
 
-        local cmd = param:sub(start,stop)
+        local cmd = param:sub(start, stop)
 
-        param =
-            param:sub(1,s-1)
-            ..param:sub(stop+1)
+        param = param:sub(1, s - 1) .. param:sub(stop + 1)
 
         return cmd
     end
@@ -64,9 +63,9 @@ local function extract_conditional_commands(param)
     exec_unless = extract("execute!")
     exec_if = extract("execute")
 
-    param = param:gsub("^%s*(.-)%s*$","%1")
+    param = param:gsub("^%s*(.-)%s*$", "%1")
 
-    return exec_if,exec_unless,param
+    return exec_if, exec_unless, param
 
 end
 
@@ -74,27 +73,29 @@ end
 -- EXECUTE RUNNER
 --------------------------------------------------
 
-local function run_conditional_command(name,cmd)
+local function run_conditional_command(name, cmd)
 
-    if not cmd then return end
+    if not cmd then
+        return
+    end
 
     for single in cmd:gmatch("[^,]+") do
 
-        single = single:gsub("^%s*(.-)%s*$","%1")
+        single = single:gsub("^%s*(.-)%s*$", "%1")
 
-        if single:sub(1,1)=="/" then
-            single=single:sub(2)
+        if single:sub(1, 1) == "/" then
+            single = single:sub(2)
         end
 
-        local args=single:split(" ")
+        local args = single:split(" ")
 
-        local command=args[1]
-        table.remove(args,1)
+        local command = args[1]
+        table.remove(args, 1)
 
-        local def=minetest.registered_chatcommands[command]
+        local def = minetest.registered_chatcommands[command]
 
         if def then
-            def.func(name,table.concat(args," "))
+            def.func(name, table.concat(args, " "))
         end
 
     end
@@ -105,14 +106,16 @@ end
 -- COORD PARSER
 --------------------------------------------------
 
-local function parse_coord(str,current)
+local function parse_coord(str, current)
 
-    if not str then return current end
+    if not str then
+        return current
+    end
 
-    if str:sub(1,1)=="~" then
+    if str:sub(1, 1) == "~" then
 
-        local offset=tonumber(str:sub(2)) or 0
-        return current+offset
+        local offset = tonumber(str:sub(2)) or 0
+        return current + offset
 
     end
 
@@ -120,20 +123,23 @@ local function parse_coord(str,current)
 
 end
 
+local function get_pos_from_args(args, player)
 
-local function get_pos_from_args(args,player)
+    local pos = player:get_pos()
 
-    local pos=player:get_pos()
+    local x = parse_coord(args[1], pos.x)
+    local y = parse_coord(args[2], pos.y)
+    local z = parse_coord(args[3], pos.z)
 
-    local x=parse_coord(args[1],pos.x)
-    local y=parse_coord(args[2],pos.y)
-    local z=parse_coord(args[3],pos.z)
+    table.remove(args, 1)
+    table.remove(args, 1)
+    table.remove(args, 1)
 
-    table.remove(args,1)
-    table.remove(args,1)
-    table.remove(args,1)
-
-    return vector.round({x=x,y=y,z=z}),args
+    return vector.round({
+        x = x,
+        y = y,
+        z = z
+    }), args
 
 end
 
@@ -141,13 +147,15 @@ end
 -- ENTITY MATCH
 --------------------------------------------------
 
-local function entity_matches(obj,filters)
+local function entity_matches(obj, filters)
 
-    local is_player=obj:is_player()
+    local is_player = obj:is_player()
 
-    local lua=obj:get_luaentity()
+    local lua = obj:get_luaentity()
 
-    if not is_player and not lua then return false end
+    if not is_player and not lua then
+        return false
+    end
 
     if filters.only_players and not is_player then
         return false
@@ -155,15 +163,13 @@ local function entity_matches(obj,filters)
 
     if filters.type then
 
-        local name =
-            is_player and "player"
-            or lua.name
+        local name = is_player and "player" or lua.name
 
-        if name~=filters.type then
+        if name ~= filters.type then
 
-            local short=name:match(":(.+)")
+            local short = name:match(":(.+)")
 
-            if short~=filters.type then
+            if short ~= filters.type then
                 return false
             end
 
@@ -179,65 +185,58 @@ end
 -- /TESTFOR
 --------------------------------------------------
 
-minetest.register_chatcommand("testfor",{
+minetest.register_chatcommand("testfor", {
 
-    params="<selector>",
-    privs={server=true},
+    params = "<selector>",
+    privs = {
+        server = true
+    },
 
-    func=function(name,param)
+    func = function(name, param)
 
-        local player=minetest.get_player_by_name(name)
+        local player = minetest.get_player_by_name(name)
 
-        local exec_if,exec_unless,param=
-            extract_conditional_commands(param)
+        local exec_if, exec_unless, param = extract_conditional_commands(param)
 
-        local selector=param
+        local selector = param
 
-        local filters={}
+        local filters = {}
 
         if selector:find("@a") then
-            filters.only_players=true
+            filters.only_players = true
         end
 
-        local type=selector:match("type=([^%]]+)")
+        local type = selector:match("type=([^%]]+)")
 
         if type then
-            filters.type=type
+            filters.type = type
         end
 
+        local center = player:get_pos()
 
-        local center=player:get_pos()
+        local objects = minetest.get_objects_inside_radius(center, 32768)
 
-        local objects=minetest.get_objects_inside_radius(center,32768)
+        local count = 0
 
-        local count=0
+        for _, obj in ipairs(objects) do
 
-        for _,obj in ipairs(objects) do
-
-            if entity_matches(obj,filters) then
-                count=count+1
+            if entity_matches(obj, filters) then
+                count = count + 1
             end
 
         end
 
-
-        local success=count>0
+        local success = count > 0
 
         set_commandblock_result(success)
 
-
         if success then
-            run_conditional_command(name,exec_if)
+            run_conditional_command(name, exec_if)
         else
-            run_conditional_command(name,exec_unless)
+            run_conditional_command(name, exec_unless)
         end
 
-
-        return success,
-        success and
-        ("Encontrado "..count)
-        or
-        "Nenhum encontrado"
+        return success, success and ("Encontrado " .. count) or "Nenhum encontrado"
 
     end
 
@@ -247,55 +246,55 @@ minetest.register_chatcommand("testfor",{
 -- /TESTFORBLOCK
 --------------------------------------------------
 
-minetest.register_chatcommand("testforblock",{
+minetest.register_chatcommand("testforblock", {
 
-params="<x> <y> <z> <node>",
+    params = "<x> <y> <z> <node>",
 
-privs={server=true},
+    privs = {
+        server = true
+    },
 
-func=function(name,param)
+    func = function(name, param)
 
-local player=minetest.get_player_by_name(name)
+        local player = minetest.get_player_by_name(name)
 
-local exec_if,exec_unless,param=
-extract_conditional_commands(param)
+        local exec_if, exec_unless, param = extract_conditional_commands(param)
 
-local args=param:split(" ")
+        local args = param:split(" ")
 
-local pos,args=
-get_pos_from_args(args,player)
+        local pos, args = get_pos_from_args(args, player)
 
-local node=args[1]
+        local node = args[1]
 
-if not node then return false end
+        if not node then
+            return false
+        end
 
-if not node:find(":") then
+        if not node:find(":") then
 
-local full="mcl_core:"..node
+            local full = "mcl_core:" .. node
 
-if minetest.registered_nodes[full] then
-node=full
-end
+            if minetest.registered_nodes[full] then
+                node = full
+            end
 
-end
+        end
 
-local current=minetest.get_node(pos)
+        local current = minetest.get_node(pos)
 
-local success=current.name==node
+        local success = current.name == node
 
-set_commandblock_result(success)
+        set_commandblock_result(success)
 
-if success then
-run_conditional_command(name,exec_if)
-else
-run_conditional_command(name,exec_unless)
-end
+        if success then
+            run_conditional_command(name, exec_if)
+        else
+            run_conditional_command(name, exec_unless)
+        end
 
-return success,
-success and "Bloco correto"
-or "Bloco diferente"
+        return success, success and "Bloco correto" or "Bloco diferente"
 
-end
+    end
 
 })
 
@@ -303,93 +302,85 @@ end
 -- /TESTFORBLOCKS
 --------------------------------------------------
 
-minetest.register_chatcommand("testforblocks",{
+minetest.register_chatcommand("testforblocks", {
 
-params="<area1> <area2>",
+    params = "<area1> <area2> destiny execute=<command>",
 
-privs={server=true},
+    privs = {
+        server = true
+    },
 
-func=function(name,param)
+    func = function(name, param)
 
-local player=minetest.get_player_by_name(name)
+        local player = minetest.get_player_by_name(name)
 
-local exec_if,exec_unless,param=
-extract_conditional_commands(param)
+        local exec_if, exec_unless, param = extract_conditional_commands(param)
 
-local args=param:split(" ")
+        local args = param:split(" ")
 
-local pos1,args=
-get_pos_from_args(args,player)
+        local pos1, args = get_pos_from_args(args, player)
 
-local pos2,args=
-get_pos_from_args(args,player)
+        local pos2, args = get_pos_from_args(args, player)
 
-local pos3,args=
-get_pos_from_args(args,player)
+        local pos3, args = get_pos_from_args(args, player)
 
+        local minx = math.min(pos1.x, pos2.x)
+        local miny = math.min(pos1.y, pos2.y)
+        local minz = math.min(pos1.z, pos2.z)
 
-local minx=math.min(pos1.x,pos2.x)
-local miny=math.min(pos1.y,pos2.y)
-local minz=math.min(pos1.z,pos2.z)
+        local maxx = math.max(pos1.x, pos2.x)
+        local maxy = math.max(pos1.y, pos2.y)
+        local maxz = math.max(pos1.z, pos2.z)
 
-local maxx=math.max(pos1.x,pos2.x)
-local maxy=math.max(pos1.y,pos2.y)
-local maxz=math.max(pos1.z,pos2.z)
+        local success = true
 
+        for x = minx, maxx do
+            for y = miny, maxy do
+                for z = minz, maxz do
 
-local success=true
+                    local offset = {
+                        x = x - minx,
+                        y = y - miny,
+                        z = z - minz
+                    }
 
+                    local src = minetest.get_node({
+                        x = x,
+                        y = y,
+                        z = z
+                    })
 
-for x=minx,maxx do
-for y=miny,maxy do
-for z=minz,maxz do
+                    local dst = minetest.get_node({
 
-local offset={
-x=x-minx,
-y=y-miny,
-z=z-minz
-}
+                        x = pos3.x + offset.x,
+                        y = pos3.y + offset.y,
+                        z = pos3.z + offset.z
 
-local src=minetest.get_node({x=x,y=y,z=z})
+                    })
 
-local dst=minetest.get_node({
+                    if src.name ~= dst.name then
+                        success = false
+                        break
+                    end
 
-x=pos3.x+offset.x,
-y=pos3.y+offset.y,
-z=pos3.z+offset.z
+                end
+            end
+        end
 
-})
+        set_commandblock_result(success)
 
-if src.name~=dst.name then
-success=false
-break
-end
+        if success then
+            run_conditional_command(name, exec_if)
+        else
+            run_conditional_command(name, exec_unless)
+        end
 
-end
-end
-end
+        return success, success and "Áreas iguais" or "Áreas diferentes"
 
-
-set_commandblock_result(success)
-
-
-if success then
-run_conditional_command(name,exec_if)
-else
-run_conditional_command(name,exec_unless)
-end
-
-
-return success,
-success and
-"Áreas iguais"
-or
-"Áreas diferentes"
-
-end
+    end
 
 })
 
 --------------------------------------------------
-minetest.log("action","[Mineclonia] testfor carregado")
+minetest.log("action", "[Mineclonia] testfor carregado")
 --------------------------------------------------
