d0f27890744a8582a8bb6a35b29d059e0ca5dcca
wrxxnch <root@Sheila.localdomain>
2026-02-25 22:35:17 -0300

    allay follows player,removed the visual item logic for now

--- mods/ENTITIES/mobs_mc/allay.lua
+++ mods/ENTITIES/mobs_mc/allay.lua
@@ -2,26 +2,6 @@ local S = core.get_translator("mobs_mc")
 local mob_class = mcl_mobs.mob_class
 
 --------------------------------------------------------
--- Entidade Visual para o Item na Mão
---------------------------------------------------------
-
-minetest.register_entity("mobs_mc:visual_item", {
-    initial_properties = {
-        visual = "wielditem",
-        textures = {"air"},
-        visual_size = {x = 0.2, y = 0.2},
-        collisionbox = {0, 0, 0, 0, 0, 0},
-        pointable = false,
-        static_save = false,
-    },
-    on_step = function(self, dtime)
-        if not self.object:get_attach() then
-            self.object:remove()
-        end
-    end,
-})
-
---------------------------------------------------------
 -- ALLAY DEFINITION
 --------------------------------------------------------
 
@@ -30,7 +10,7 @@ local allay = {
     type = "animal",
     spawn_class = "passive",
     passive = true,
-
+    glow = 5,
     hp_min = 20,
     hp_max = 20,
 
@@ -52,34 +32,45 @@ local allay = {
     _apply_gravity_drag_on_ground = false,
     movement_speed = 3.0,
     view_range = 16,
+
+    lifetimer = -1,
+    static_save = true,
+    despawn = false,
+
+    --------------------------------------------------------
+    -- PERSISTÊNCIA
+    --------------------------------------------------------
+
+    on_activate = function(self, staticdata, dtime_s)
+        if staticdata and staticdata ~= "" then
+            local data = minetest.deserialize(staticdata)
+            if data then
+                self._given_item = data._given_item
+                self._given_stack = data._given_stack
+                self._picked_up_item = data._picked_up_item
+                self._player = data._player
+            end
+        end
+    end,
+
+    get_staticdata = function(self)
+        return minetest.serialize({
+            _given_item = self._given_item,
+            _given_stack = self._given_stack,
+            _picked_up_item = self._picked_up_item,
+            _player = self._player,
+        })
+    end,
 }
 
 --------------------------------------------------------
 -- SISTEMA DE ITENS
 --------------------------------------------------------
 
-function allay:_update_hand_item()
-    if self._wielded_item_entity then
-        self._wielded_item_entity:remove()
-        self._wielded_item_entity = nil
-    end
-
-    local item_to_show = self._picked_up_item or self._given_stack
-    if item_to_show then
-        local pos = self.object:get_pos()
-        local ent = minetest.add_entity(pos, "mobs_mc:visual_item")
-        if ent then
-            ent:set_properties({
-                textures = {ItemStack(item_to_show):get_name()},
-            })
-            ent:set_attach(self.object, "Arm_R", {x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
-            self._wielded_item_entity = ent
-        end
-    end
-end
-
 function allay:_drop_items(only_picked_up)
     local pos = self.object:get_pos()
+    if not pos then return end
+
     if self._picked_up_item then
         local item_obj = minetest.add_item(pos, self._picked_up_item)
         if item_obj then
@@ -94,22 +85,23 @@ function allay:_drop_items(only_picked_up)
         self._given_item = nil
         self._player = nil
     end
-    self:_update_hand_item()
 end
 
 function allay:on_rightclick(clicker)
     local wi = clicker:get_wielded_item()
+
     if not self._given_item and not wi:is_empty() then
         self._player = clicker:get_player_name()
         self._given_item = wi:get_name()
         self._given_stack = wi:to_string()
+
         if not minetest.settings:get_bool("creative_mode") then
             wi:take_item()
             clicker:set_wielded_item(wi)
         end
-        self:_update_hand_item()
         return
     end
+
     self:_drop_items()
 end
 
@@ -118,16 +110,14 @@ end
 --------------------------------------------------------
 
 function allay:motion_step(dtime, moveresult, self_pos)
-    -- Manter item na mão se necessário
-    if (self._given_item or self._picked_up_item) and 
-       (not self._wielded_item_entity or not self._wielded_item_entity:get_luaentity()) then
-        self:_update_hand_item()
-    end
 
     local target_pos = nil
     local player = self._player and minetest.get_player_by_name(self._player)
 
-    -- 1. BUSCAR ITEM NO CHÃO
+    --------------------------------------------------------
+    -- BUSCAR ITEM NO CHÃO
+    --------------------------------------------------------
+
     if self._given_item and not self._picked_up_item then
         local items = minetest.get_objects_inside_radius(self_pos, self.view_range)
         for _, o in ipairs(items) do
@@ -139,7 +129,6 @@ function allay:motion_step(dtime, moveresult, self_pos)
                     if vector.distance(self_pos, opos) < 1.5 then
                         self._picked_up_item = entity.itemstring
                         o:remove()
-                        self:_update_hand_item()
                     else
                         target_pos = opos
                         break
@@ -149,64 +138,57 @@ function allay:motion_step(dtime, moveresult, self_pos)
         end
     end
 
-    -- 2. SEGUIR JOGADOR (Se tiver item coletado OU apenas o item dado)
-    if not target_pos and player then
+    --------------------------------------------------------
+    -- SEGUIR JOGADOR + TELEPORTE
+    --------------------------------------------------------
+
+    if player then
         local ppos = player:get_pos()
-        ppos.y = ppos.y + 1.2 -- Altura do peito/cabeça do jogador
+        ppos.y = ppos.y + 1.2
         local dist = vector.distance(self_pos, ppos)
-        
+
+        if dist > 20 then
+            self.object:set_pos(vector.offset(ppos, math.random(-1,1), 0, math.random(-1,1)))
+            return
+        end
+
         if self._picked_up_item then
-            -- Se coletou item, segue bem de perto (para entregar ou apenas acompanhar)
             if dist > 2.0 then
                 target_pos = ppos
             end
         elseif self._given_item then
-            -- Se apenas tem o item de referência, segue a uma distância amigável
             if dist > 3.5 then
                 target_pos = ppos
             end
         end
     end
 
-    -- 3. MOVIMENTO ALEATÓRIO (Wander)
+    --------------------------------------------------------
+    -- WANDER
+    --------------------------------------------------------
+
     if not target_pos then
         if not self._wander_pos or vector.distance(self_pos, self._wander_pos) < 1.5 or math.random(100) == 1 then
-            -- Wander limitado perto do jogador se ele existir
             local base_pos = player and player:get_pos() or self_pos
             self._wander_pos = vector.offset(base_pos, math.random(-4, 4), math.random(1, 2), math.random(-4, 4))
-            
-            -- Verificar se o local é válido
-            local node = core.get_node(self._wander_pos)
-            if core.registered_nodes[node.name] and core.registered_nodes[node.name].walkable then
+
+            local node_at_pos = core.get_node(self._wander_pos)
+            if core.registered_nodes[node_at_pos.name] and core.registered_nodes[node_at_pos.name].walkable then
                 self._wander_pos = nil
             end
         end
         target_pos = self._wander_pos
     end
 
-    -- CÁLCULO DE VELOCIDADE E LIMITES
-    if target_pos then
-        -- 🔥 LIMITE DE ALTURA: Não subir mais que 4 blocos acima do chão
-        local check_pos = {x=self_pos.x, y=self_pos.y, z=self_pos.z}
-        local dist_to_ground = 0
-        for i=1, 5 do
-            local n = core.get_node({x=check_pos.x, y=check_pos.y-i, z=check_pos.z})
-            if core.registered_nodes[n.name] and core.registered_nodes[n.name].walkable then
-                dist_to_ground = i
-                break
-            end
-        end
-        
-        -- Se estiver muito alto, força o alvo para baixo
-        if dist_to_ground == 0 or dist_to_ground > 4 then
-             target_pos.y = self_pos.y - 1
-        end
+    --------------------------------------------------------
+    -- MOVIMENTO SUAVE
+    --------------------------------------------------------
 
+    if target_pos then
         local dir = vector.direction(self_pos, target_pos)
-        local target_speed = 3.0
-        local target_vel = vector.multiply(dir, target_speed)
+        local target_vel = vector.multiply(dir, 3.0)
         local current_vel = self.object:get_velocity()
-        
+
         local smooth = 0.08
         local new_vel = {
             x = current_vel.x + (target_vel.x - current_vel.x) * smooth,
@@ -214,13 +196,8 @@ function allay:motion_step(dtime, moveresult, self_pos)
             z = current_vel.z + (target_vel.z - current_vel.z) * smooth,
         }
 
-        -- Altura mínima (não encostar no chão)
-        if dist_to_ground <= 1 and new_vel.y < 0 then
-            new_vel.y = 0.5
-        end
-
         self.object:set_velocity(new_vel)
-        
+
         if vector.length(new_vel) > 0.1 then
             local yaw = math.atan2(new_vel.z, new_vel.x) - math.pi / 2
             self:set_yaw(yaw)
@@ -232,13 +209,10 @@ function allay:motion_step(dtime, moveresult, self_pos)
 end
 
 function allay:run_ai(dtime, moveresult)
-    return 
+    return
 end
 
 function allay:on_die(pos)
-    if self._wield_item_entity then
-        self._wield_item_entity:remove()
-    end
     self:_drop_items()
 end
 
