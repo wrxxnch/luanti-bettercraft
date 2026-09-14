a7d3b6f68bd8da28153d41d9a377acb48731860c
wrxxnch <7.3299769e+07+wrxxnch@users.noreply.github.com>
2026-09-14 04:25:48 +0000

    Fix map creation and cartography markers

--- games/bettercraft/mods/ITEMS/mcl_maps/init.lua
+++ games/bettercraft/mods/ITEMS/mcl_maps/init.lua
@@ -42,251 +42,7 @@ end
 
 local texture_colors = load_json_file("colors")
 local palettes = load_json_file("palettes")
-
-local color_cache = {}
-
-local creating_maps = {}
-local loaded_maps = {}
-
-local c_air = core.get_content_id("air")
-
-function mcl_maps.create_map(pos)
-	local minp = vector.multiply(vector.floor(vector.divide(pos, 128)), 128)
-	local maxp = vector.add(minp, vector.new(127, 127, 127))
-
-	local itemstack = ItemStack("mcl_maps:filled_map")
-	local meta = itemstack:get_meta()
-	local next_id = storage:get_int("next_id")
-	storage:set_int("next_id", next_id + 1)
-	local id = tostring(next_id)
-	meta:set_string("mcl_maps:id", id)
-	meta:set_string("mcl_maps:minp", core.pos_to_string(minp))
-	meta:set_string("mcl_maps:maxp", core.pos_to_string(maxp))
-	meta:set_int("date", os.time())
-	tt.reload_itemstack_description(itemstack)
-
-	creating_maps[id] = true
-	core.emerge_area(minp, maxp, function(_, _, calls_remaining)
-		if calls_remaining > 0 then
-			return
-		end
-		local vm = core.get_voxel_manip()
-		local emin, emax = vm:read_from_map(minp, maxp)
-		local data = vm:get_data()
-		local param2data = vm:get_param2_data()
-		local area = VoxelArea:new({ MinEdge = emin, MaxEdge = emax })
-		local pixels = {}
-		local last_heightmap
-		for x = 1, 128 do
-			local map_x = minp.x - 1 + x
-			local heightmap = {}
-			for z = 1, 128 do
-				local map_z = minp.z - 1 + z
-				local color, height
-				for map_y = maxp.y, minp.y, -1 do
-					local index = area:index(map_x, map_y, map_z)
-					local c_id = data[index]
-					if c_id ~= c_air then
-						color = color_cache[c_id]
-						if color == nil then
-							local nodename = core.get_name_from_content_id(c_id)
-							local def = core.registered_nodes[nodename]
-							if def then
-								local texture
-								if def.palette and def.palette ~= "" and ( def.paramtype2 == "color" or
-									def.paramtype2 == "colorwallmounted" or def.paramtype2 == "colorfacedir" or
-									def.paramtype2 == "color4dir" ) then
-									texture = def.palette
-								elseif def.tiles then
-									texture = def.tiles[1]
-									if type(texture) == "table" then
-										texture = texture.name
-									end
-								end
-								if texture then
-									texture = texture:match("([^=^%^]-([^.]+))$"):split("^")[1]
-								end
-								if def.palette and def.palette ~= "" and ( def.paramtype2 == "color" or
-									def.paramtype2 == "colorwallmounted" or def.paramtype2 == "colorfacedir" or
-									def.paramtype2 == "color4dir" ) then
-									local palette = palettes[texture]
-									color = palette and { palette = palette }
-								elseif texture_colors then
-									color = texture_colors[texture]
-								end
-							end
-						end
-
-						if color and color.palette then
-							color = color.palette[param2data[index] + 1]
-						else
-							color_cache[c_id] = color or false
-						end
-
-						if color and last_heightmap then
-							local last_height = last_heightmap[z]
-							if last_height < map_y then
-								color = {
-									math.min(255, color[1] + 16),
-									math.min(255, color[2] + 16),
-									math.min(255, color[3] + 16),
-								}
-							elseif last_height > map_y then
-								color = {
-									math.max(0, color[1] - 16),
-									math.max(0, color[2] - 16),
-									math.max(0, color[3] - 16),
-								}
-							end
-						end
-						height = map_y
-						break
-					end
-				end
-				heightmap[z] = height or minp.y
-				pixels[z] = pixels[z] or {}
-				pixels[z][x] = color or { 0, 0, 0 }
-			end
-			last_heightmap = heightmap
-		end
-		tga_encoder.image(pixels):save(map_textures_path .. "mcl_maps_map_texture_" .. id .. ".tga", { compression = "RLE", color_format = "A1R5G5B5", })
-		creating_maps[id] = nil
-	end)
-	return itemstack
-end
-
-function mcl_maps.load_map(id, callback)
-	if not id or id == "" or creating_maps[id] then
-		return false
-	end
-
-	local texture = "mcl_maps_map_texture_" .. id .. ".tga"
-
-	local result = true
-
-	if not loaded_maps[id] then
-		if not core.features.dynamic_add_media_table then
-			-- core.dynamic_add_media() blocks in
-			-- Minetest 5.3 and 5.4 until media loads
-			loaded_maps[id] = true
-			result = core.dynamic_add_media(map_textures_path .. texture, function()
-			end)
-			if callback then
-				callback(texture)
-			end
-		else
-			-- core.dynamic_add_media() never blocks
-			-- in Minetest 5.5, callback runs after load
-			result = core.dynamic_add_media(map_textures_path .. texture, function()
-				loaded_maps[id] = true
-				if callback then
-					callback(texture)
-				end
-			end)
-		end
-	end
-
-	if result == false then
-		return false
-	end
-
-	if loaded_maps[id] then
-		if callback then
-			callback(texture)
-		end
-		return texture
-	end
-end
-
-local function fill_map(itemstack, placer, pointed_thing)
-	local new_stack = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
-	if new_stack then
-		return new_stack
-	end
-
-	if core.settings:get_bool("enable_real_maps", true) then
-		local new_map = mcl_maps.create_map(placer:get_pos())
-		itemstack:take_item()
-		if itemstack:is_empty() then
-			return new_map
-		else
-			local inv = placer:get_inventory()
-			if inv:room_for_item("main", new_map) then
-				inv:add_item("main", new_map)
-			else
-				core.add_item(placer:get_pos(), new_map)
-			end
-			return itemstack
-		end
-	end
-end
-
-core.register_craftitem("mcl_maps:empty_map", {
-	description = S("Empty Map"),
-	_doc_items_longdesc = S("Empty maps are not useful as maps, but they can be stacked and turned to maps which can be used."),
-	_doc_items_usagehelp = S("Rightclick to create a filled map (which can't be stacked anymore)."),
-	inventory_image = "mcl_maps_map_empty.png",
-	on_place = fill_map,
-	on_secondary_use = fill_map,
-})
-
-local filled_def = {
-	description = S("Map"),
-	_tt_help = S("Shows a map image."),
-	_doc_items_longdesc = S("When created, the map saves the nearby area as an image that can be viewed any time by holding the map."),
-	_doc_items_usagehelp = S("Hold the map in your hand. This will display a map on your screen."),
-	inventory_image = "mcl_maps_map_filled.png^(mcl_maps_map_filled_markings.png^[colorize:#000000)",
-	groups = { not_in_creative_inventory = 1, filled_map = 1, tool = 1 },
-}
-
-core.register_craftitem("mcl_maps:filled_map", filled_def)
-
-tt.register_priority_snippet(function(itemstring, _, itemstack)
-	if itemstack and core.get_item_group(itemstring, "filled_map") > 0 then
-		local id = itemstack:get_meta():get_string("mcl_maps:id")
-		if id ~= "" then
-			return "#" .. id, mcl_colors.GRAY
-		end
-	end
-end)
-
-core.register_craft({
-	output = "mcl_maps:empty_map",
-	recipe = {
-		{ "mcl_core:paper", "mcl_core:paper", "mcl_core:paper" },
-		{ "mcl_core:paper", "group:compass", "mcl_core:paper" },
-		{ "mcl_core:paper", "mcl_core:paper", "mcl_core:paper" },
-	}
-})
-
-core.register_craft({
-	type = "shapeless",
-	output = "mcl_maps:filled_map 2",
-	recipe = { "group:filled_map", "mcl_maps:empty_map" },
-})
-
-local function on_craft(itemstack, _, old_craft_grid, _)
-	if itemstack:get_name() == "mcl_maps:filled_map" then
-		for _, stack in pairs(old_craft_grid) do
-			if core.get_item_group(stack:get_name(), "filled_map") > 0 then
-				itemstack:get_meta():from_table(stack:get_meta():to_table())
-				return itemstack
-			end
-		end
-	end
-end
-
-core.register_on_craft(on_craft)
-core.register_craft_predict(on_craft)
-
-local enable_real_maps
-	= core.settings:get_bool ("enable_real_maps", true)
-local enable_minimap_shading
-	= core.settings:get_bool ("mcl_enable_minimap_map_shading", false)
-
-------------------------------------------------------------------------
--- Dynamically updated maps.
-------------------------------------------------------------------------
+local storage = core.get_mod_storage ()
 
 local map_colors_by_cid = {}
 
@@ -360,6 +116,23 @@ local map_get_node_raw = core.get_node_raw
 
 local function get_rgb (cid, param2)
 	local color_or_list = map_colors_by_cid[cid]
+	if color_or_list == nil then
+		local name = core.get_name_from_content_id (cid)
+		local def = core.registered_nodes[name]
+		local texture = def and def.tiles and def.tiles[1]
+		if type (texture) == "table" then
+			texture = texture.name
+		end
+		if texture then
+			texture = texture:match ("[^%^]+")
+			texture = texture:match ("[^/]+$") or texture
+			local color = texture_colors[texture]
+			if color and type (color[1]) == "number" then
+				color_or_list = encode_rgb (color[1], color[2], color[3])
+				map_colors_by_cid[cid] = color_or_list
+			end
+		end
+	end
 	if type (color_or_list) == "number" then
 		return color_or_list
 	elseif color_or_list then
@@ -373,7 +146,9 @@ local function scan_heightmap (current_height, x, z, y1, y2)
 	if y2 >= current_height then
 		for y = y2, y1, -1 do
 			local cid, _, _ = map_get_node_raw (x, y, z)
-			if map_colors_by_cid[cid] then
+			local name = core.get_name_from_content_id (cid)
+			local def = core.registered_nodes[name]
+			if map_colors_by_cid[cid] or (def and def.tiles) then
 				return y
 			end
 
@@ -1511,7 +1286,6 @@ mcl_maps.register_explorer_map ("mcl_maps:jungle_explorer_map", "#848484", {
 	_skip_existing_chunks = true,
 })
 
-local storage = core.get_mod_storage ()
 
 function mcl_maps.initialize_explorer_map (pos, stack)
 	local def = core.registered_items[stack:get_name ()]
